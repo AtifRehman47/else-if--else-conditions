1e6b9aa49247c4a53a0c9d925759786f22fd7c0a
Atif Rehman <atifrehman401@gmail.com>
2024-02-29 19:22:22 +0000

    else if ,else conditions

---
+++ app.js
@@ -0,0 +1,63 @@
+// var persontage=+prompt("Enter your prsontage");
+// if (persontage>100) {
+//     console.log("persontage to sahi do")
+// }
+// else if (persontage<60); {
+//     console.log("B")
+// }
+
+// else(persontage<50); {
+//     console.log("malik fail ho gaya")
+// }
+
+
+// var per =50
+
+// if (per>=80 && per<=100) {
+//     console.log("your marit.");
+// }
+// else if (per>=60  && per<80) {
+//     console.log("you are in 1st devision");
+// }
+// else if (per>=45  && per<60) {
+//     console.log("second devision");
+// }
+// else if (per>=33  && per<40) {
+//     console.log("fail");
+// }
+
+// var  BALANCE =1500000
+
+// if ( BALANCE>500000) {
+//     console.log('rista mubarak');
+// }
+// else if ( BALANCE >25) {
+//     console.log('rista war gaya');
+// }
+
+// var age =20
+// if (age>18) {
+//     console.log("you can drink water");
+// }
+// else{
+//     console.log("you can drink coldrink");
+// }
+
+
+//  elseif multiple conditions k liye hota ha
+// jese k var age ki jagha par aghar age 20 likho to nic show hoga, aghar age ki jagha par 2,5 liko to age show hogi)
+
+var age = 20
+if (age>17) {
+    console.log("show NIC");
+}
+else if (age==2) {
+    console.log("age is 2");
+}
+else if (age==5) {
+    console.log("age is 5");
+}
+else{
+    console.log("not show NIC")
+}
+
